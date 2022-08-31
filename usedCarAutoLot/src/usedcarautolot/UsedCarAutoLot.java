a31fb2bd24de221a928f3bf722c2c7b344ef7a5f
Kevin Bell <bellKevin@pm.me>
2022-08-31 14:06:25 -0600

    Add files via upload

---
+++ usedCarAutoLot/src/usedcarautolot/UsedCarAutoLot.java
@@ -0,0 +1,14 @@
+/** ****************************************************************************
+ * UsedCarAutoLot.java
+ * Kevin Bell
+ *
+ * This driver program simulates a Used Car Auto Lot.
+ **************************************************************************** */
+package usedcarautolot;
+
+public class UsedCarAutoLot {
+
+    public static void main(String[] args) {
+        System.out.println("Chapter 14 Used Car Auto Lot by Kevin Bell");
+    } // end main method
+} // end UsedCarAutoLot class
