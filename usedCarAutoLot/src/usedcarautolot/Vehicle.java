d037284b516f2a1e8046adcb8f3391754cec5cd4
Kevin Bell <bellKevin@pm.me>
2022-09-01 00:48:59 +0000

    Add files via upload

---
+++ usedCarAutoLot/src/usedcarautolot/Vehicle.java
@@ -0,0 +1,13 @@
+/** ****************************************************************************
+ * Vehicle.java
+ * Kevin Bell
+ *
+ * This base/super program simulates a Vehicle.
+ **************************************************************************** */
+package usedcarautolot;
+
+class Vehicle {
+
+    public int inventoryID, year, mileage;
+    public String make, model, color;
+} // end Vehicle class
